378970edd63ce052cf429498203b3cbf9697e7c9
TechWiz-3 <programmingplus1@gmail.com>
2022-07-19 15:41:03 +1000

    🎉 INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pygnore
+
+Super simple python CLI tool to generate a gitignore using the github python gitignore template.
+
+## Installatiion
+
+```py
+pip install pygnore
+```
+
+## Usage
+
+```
+pygnore
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pygnore-cli',  # pip install
+    version='1.0.0',
+    description='Generate gitignore file using the github ignore template',
+    scripts=['pygnore'],
+)
