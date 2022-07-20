1edabfce9eef21259ba3823e054ca5e93d1d180e
TechWiz-3 <programmingplus1@gmail.com>
2022-07-20 12:44:36 +1000

    👌 IMPROVE: working version

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pygnore-cli',  # pip install
     version='1.0.0',
     description='Generate gitignore file using the github ignore template',
+    packages=find_packages(where="."),
     scripts=['pygnore'],
 )
